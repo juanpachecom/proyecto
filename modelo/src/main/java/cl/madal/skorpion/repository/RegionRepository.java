2fa6e257da898f7d8298694cc520ac7058f40000
Sebastián Salazar Molina <sebasalazar@gmail.com>
2015-07-04 13:25:55 -0300

    Creación de repositorios

---
+++ modelo/src/main/java/cl/madal/skorpion/repository/RegionRepository.java
@@ -0,0 +1,14 @@
+package cl.madal.skorpion.repository;
+
+import cl.madal.skorpion.modelo.Region;
+import javax.annotation.Resource;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author Sebastián Salazar Molina <sebasalazar@gmail.com>
+ */
+@Resource(name = "regionRepository")
+public interface RegionRepository extends JpaRepository<Region, Integer> {
+
+}
